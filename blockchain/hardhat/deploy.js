fc5724e87fcffa81b600a37296646935a9d3304d
Dalos Network <125082532+dalosnetwork@users.noreply.github.com>
2025-02-20 16:36:20 +0300

    Update deploy.js

--- blockchain/hardhat/deploy.js
+++ blockchain/hardhat/deploy.js
@@ -2,17 +2,17 @@
 // ---------------------------------------------
 // 1. Hardhat projesini başlattığınızdan emin olun
 // 2. `npx hardhat compile` komutu ile kontratınızı derleyin
-// 3. Deploy etmek için: `npx hardhat run scripts/deploy.js --network arbitrum_sepolia` (arbitrum sepolia için)
+// 3. Deploy etmek için: `npx hardhat run scripts/deploy.js --network networkname`
 // ---------------------------------------------
 
 const hre = require("hardhat");
 
 async function main() {
-    console.log("Deploying contract to Arbitrum Sepolia...");
+    console.log("Deploying contract to...");
 
     const MyContract = await hre.ethers.getContractFactory("MyContract");
 
-    const myContract = await MyContract.deploy("Merhaba Arbitrum!"); // contructor param
+    const myContract = await MyContract.deploy("Hi!"); // contructor param
 
     await myContract.deployed();
 
